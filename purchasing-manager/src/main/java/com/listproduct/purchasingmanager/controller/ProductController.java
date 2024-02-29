1ea6723c8315f34872f008cde19c3bf4bcde9d9a
heitorbp96 <heitorbragapereira96@gmail.com>
2024-02-28 22:39:11 -0300

    wip

--- purchasing-manager/src/main/java/com/listproduct/purchasingmanager/controller/ProductController.java
+++ purchasing-manager/src/main/java/com/listproduct/purchasingmanager/controller/ProductController.java
@@ -2,26 +2,41 @@ package com.listproduct.purchasingmanager.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.listproduct.purchasingmanager.model.Product;
 import com.listproduct.purchasingmanager.model.ProductRepository;
+import com.listproduct.purchasingmanager.service.ProductServices;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("product")
 public class ProductController {
 
-	@Autowired
-	private ProductRepository repository;
+	private final ProductRepository repository;
+	private final ProductServices productServices;
+
+	public ProductController(ProductRepository repository, ProductServices productServices) {
+		this.repository = repository;
+		this.productServices = productServices;
+	}
 
 	@GetMapping
 	public List<Product> getAllProducts() {
 		List<Product> productList = repository.findAll();
 		return productList;
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<Product> registerProduct(@RequestBody Product product) {
+		Product newProduct = productServices.registerProduct(product);
+		return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
 	}
 }
